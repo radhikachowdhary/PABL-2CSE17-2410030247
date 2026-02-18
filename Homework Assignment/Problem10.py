c4ed08476b459af8bf0bd59e06b942a97a9e15ae
radhikachowdhary <radhikachowdhary5531@gmail.com>
2026-02-19 00:19:53 +0530

    Create Problem10.py

---
+++ Homework Assignment/Problem10.py
@@ -0,0 +1,27 @@
+def min_jumps(arr):
+    n = len(arr)
+      if n <= 1:
+      return 0
+     if arr[0] == 0:
+        return -1
+
+    jumps = 0
+    current_end = 0
+    farthest = 0
+
+ for i in range(n - 1):
+       
+        farthest = max(farthest, i + arr[i])
+  if i == current_end:
+            jumps += 1
+            current_end = farthest 
+            
+           if current_end >= n - 1:
+                break
+                if current_end < n - 1:
+        return -1
+        
+    return jumps
+
+
+print(min_jumps([2, 3, 1, 1, 4]))
